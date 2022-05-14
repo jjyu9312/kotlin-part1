aae7bace05b02af5ced181249d45e4cf0be0ed71
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-14 15:58:57 +0900

    22/05/14
    코틀린 기본 자료형 파악

---
+++ src/main/kotlin/chap02.section2/StringTest.kt
@@ -0,0 +1,10 @@
+package chap02.section2
+
+fun main() {
+    var str1: String = "hello"
+    var str2 = "World"
+    var str3 = "hello"
+
+    println("str1 == str2 : ${str1 === str2}") // 메모리 공간 다름
+    println("str1 == str3 : ${str1 === str3}") // 변수 이름만 다르고 메모리 공간은 같음
+}
