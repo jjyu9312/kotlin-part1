5ed60aba06243f70b4fef9de51f86a2c3724555b
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-16 00:52:37 +0900

    22/05/16
    코틀린 기본 자료형 파악

---
+++ src/main/kotlin/chap02/section3/NullTest.kt
@@ -0,0 +1,11 @@
+package chap02.section3
+
+fun main() {
+    var str1: String?
+    str1 = null
+    val len = str1?.length ?: -1 // elvis 연산자 사용
+    println("str1 = ${str1}, length = ${str1?.length}") // ?가 붙으면 null일 경우 뒤부분 실행 안함
+    println("str1 = ${str1}, length = ${len}")
+    println()
+
+}
